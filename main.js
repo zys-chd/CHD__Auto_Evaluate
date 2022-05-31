04641673987a01a41477c26920bfe7d050ae270a
zys-chd <57886262+zys-chd@users.noreply.github.com>
2022-06-01 00:24:31 +0800

    add feature and experience improvements
    remove need for click the confirm button at the end, and move the script button to a less obstructive position.
    最后一步不需要再单击确认按钮，而且将脚本的按钮移植比较不影响视野的位置。

--- main.js
+++ main.js
@@ -1,102 +1,87 @@
 // ==UserScript==
-// @name         CHD__Auto_Judge_Educate
-// @namespace    https://github.com/zys-chd/CHD__Auto_Judge_Evaluate
-// @version      0.1
-// @description  长安大学教务系统自动评教
+// @name         长安大学教务系统自动评教
+// @namespace    https://github.com/zys-chd/CHD__Auto_Judge_Educate
+// @version      0.20
+// @description  简单的长安大学教务系统自动评教脚本
 // @author       zys
 // @match        http://bkjw.chd.edu.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chd.edu.cn
-// @grant        none
-// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @grant        unsafeWindow
+// @license      MIT
 // ==/UserScript==
-this.$ = this.jQuery = jQuery.noConflict(true);
 
 (function () {
-    
-    // var confirm=function(){return 1}
+    // 替换原有confirm函数
+    unsafeWindow.confirm = (a) => function (a) { console.log(a); return 0 };
+
     'use strict';
 
-    // Your code here...
-    // while(window.location.href!="http://bkjw.chd.edu.cn/eams/quality/stdEvaluate.action") {
-    // if (window.location.href=="http://bkjw.chd.edu.cn/eams/quality/stdEvaluate.action") {
-    //     break;
-    // }
-    // }
+    // 添加按钮设置样式并设置回调函数
+    var TampermonkeyButton = document.createElement("button")
+    TampermonkeyButton.id = "TampermonkeyButton"
+    TampermonkeyButton.textContent = "一键评教"
+    TampermonkeyButton.style.width = "120px"
+    TampermonkeyButton.style.height = "auto"
+    TampermonkeyButton.style.position = "absolute"
+    TampermonkeyButton.style.right = "20px"
 
-    // 添加按钮并设置回调函数
-    var TampermonkeyButton = document.createElement("button");
-    TampermonkeyButton.id = "TampermonkeyButton";
-    TampermonkeyButton.textContent = "一键评教";
-    TampermonkeyButton.style.width = "120px";
-    TampermonkeyButton.style.height = "50px";
-    TampermonkeyButton.style.align = "center";
+    TampermonkeyButton.onclick = ThisMain
 
-    TampermonkeyButton.onclick = ThisMain;
+    document.getElementsByClassName("banner")[0].appendChild(TampermonkeyButton)
 
-    var banner = document.getElementsByClassName("banner")[0];
-    banner.appendChild(TampermonkeyButton);
-
-    // window.onload = load;
 })();
-
-// function log(any str) {
-//     console.log(str)
-// }
 
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
+
 function ThisMain() {
+    check()
 
-    check(); // 第一版不想努力了，还要手动点进去。。。。
+    //TODO: 一步到位
+    // try {
+    //     let hrefList = document.getElementsByClassName("eval"); // 获取每个需要评教的对象如教师名称
+    //         if (hrefList.length==0) {
+    //             throw TypeError
+    //         }
+    //         console.log(hrefList.length)
+    //         for (let index = 0; index < hrefList.length; index++) {
+    //             // console.log("2")
+    //             hrefList[index].click()// 点击评教
+    //             sleep(1500).then(()=>{
+    //                 check()
+    //             });
+    //             sleep(1500).then(()=>{
+                    
+    //             })
+    //         }
+    // } catch (error) {
+    //     alert(error)
+    // }
 
-    // console.log("1");
-    // let hrefList = document.getElementsByClassName("eval"); // 获取每个需要评教的对象如教师名称
-    // console.log(hrefList.length)
-    // for (let index = 0; index < hrefList.length; index++) {
-    //     console.log("2");
-    //     hrefList[index].click();// 点击评教
-
-
-    // setTimeout(() => function () {
-    //     let judgeOptions = document.getElementsByClassName("option-radio"); // 获取所有可以选择的对象如选择框，输入框
-    //     let temp = 0;
-    //     console.log("3");
-    //     console.log(judgeOptions.length)
-    //     for (let i = 0; i < 10; i++) {
-    //         judgeOptions[temp].click();
-    //         temp = temp + 5;
-    //     }
-    //     console.log("4");
-    //     let inputText = document.getElementsByClassName("answer");
-    //     for (let j = 0; j < inputText.length; j++) {
-    //         inputText[j].value = "无";
-    //     }
-    //     console.log("5");
-    //     document.getElementById("sub").click();
-    // }, 1000)
-
-
-    return 0;
+    return 0
 }
 
+
 function check() {
-    let judgeOptions = document.getElementsByClassName("option-radio"); // 获取所有可以选择的对象如选择框，输入框
-    let temp = 0;
-    console.log("3");
-    console.log(judgeOptions.length);
-    for (let i = 0; i < 10; i++) {
-        judgeOptions[temp].click();
-        temp = temp + 5;
+    try {
+        let judgeOptions = document.getElementsByClassName("option-radio") // 获取所有可以选择的对象如选择框，输入框
+        let temp = 0
+        for (let i = 0; i < 10; i++) { // 一共十个选择题，每个有5个选项，将他们相应勾选上
+            judgeOptions[temp].click()
+            temp = temp + 5
+        }
+        let inputText = document.getElementsByClassName("answer") // 获取所有的填空题的input
+        for (let j = 0; j < inputText.length; j++) { // 将填空题的输入区填充为“无”
+            inputText[j].value = "无"
+        }
+        sleep(200).then(() => {
+            document.getElementById("sub").click() // 点击提交
+        });
+    } catch (TypeError) {
+        // 若界面未加载或者未进入对应界面则弹出提示
+        alert("请进入评教填写界面以进行一键评教！")
     }
-    console.log("4");
-    let inputText = document.getElementsByClassName("answer");
-    for (let j = 0; j < inputText.length; j++) {
-        inputText[j].value = "无";
-    }
-    console.log("5");
-    sleep(200).then(()=>{
-        document.getElementById("sub").click();
-    })
-    return 0;
+
+    return 0
 }
